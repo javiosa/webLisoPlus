bd722c64e6ef5f6b031eda45808c7c150e6a709a
javiosa <javiosa9@gmail.com>
2021-07-01 19:54:44 +0200

    fix api_key

--- src/app/services/disgenet-api.service.ts
+++ src/app/services/disgenet-api.service.ts
@@ -7,10 +7,12 @@ import {Observable} from 'rxjs';
 })
 export class DisgenetAPIService {
 
+  private api_key = 'ba82a39866de26e73bd142c8670eb8c3d96e612b '
   private headers = new HttpHeaders()
     // .set('content-type', 'application/json')
     .set('Cache-Control', 'no-cache')
     .set('Pragma', 'no-cache')
+    .set('Authorization', `Bearer ${this.api_key}`)
     .set('Access-Control-Allow-Origin', '*');
 
   private REST_API_SERVER = 'api/';
